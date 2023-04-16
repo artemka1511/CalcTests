8ebb1b275d08ea6fe4be9ec699af31557dc96910
artemka1511 <artemka15111992@gmail.com>
2023-04-17 00:56:28 +0700

    first commit

---
+++ SuperCalc.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCalc.Tests
+{
+    [TestFixture]
+    class CalculatorTests
+    {
+        public Calculator calculator = new Calculator();
+
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Assert.That(10 == calculator.Additional(2,8));
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            Assert.That(5 == calculator.Subtraction(10, 5));
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            Assert.That(64 == calculator.Miltiplication(8, 8));
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            Assert.That(9 == calculator.Division(18, 2));
+        }
+
+        [Test]
+        public void DivisionMustThrowDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(5, 0));
+        }
+    }
+}
